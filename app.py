f2e8037d4dd9d0d49e8536452c07bb2f44ba8470
zzzhenekkk <89052742425@mail.ru>
2021-10-31 13:29:33 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from aiogram import executor
+from aiogram import executor #В ней лежит старт пулинг, с помощью которого мы запускаем бота
 
-from loader import dp
+from loader import dp #С лоадера в этом проекте испортируем диспатчер - доставщика наших обновлений
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -15,5 +15,5 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup) #Эта функция запускает бота
 
