01043e6c82f457051cab2af830f4c207644673e4
zzzhenekkk <89052742425@mail.ru>
2021-10-31 13:51:59 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from aiogram import executor #В ней лежит старт пулинг, с помощью которого мы запускаем бота
 
-from loader import dp #С лоадера в этом проекте испортируем диспатчер - доставщика наших обновлений
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
+from loader import dp #С лоадера в этом проекте испортируем диспатчер - доставщика наших обновлений, сообщений и проч к обработчику
+import middlewares, filters, handlers #Импортируются с нашего проекта, модули в таком порядке, чтобы они корректно работали
+from utils.notify_admins import on_startup_notify #Оповещение администраторов, что бот запущен
+from utils.set_bot_commands import set_default_commands #Функция установки дефолтных команд для бота
 
 
 async def on_startup(dispatcher):
